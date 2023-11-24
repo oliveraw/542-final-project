34fb42767722aefdf82bf538f926d5ae23319421
oliveraw <oliveraw@umich.edu>
2023-11-24 15:32:15 -0500

    initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+ROOT_DIR = os.path.join("./")
+DATA_DIR = os.path.join(ROOT_DIR, "data/waic-tsc")
+
+NUM_FRAMES = 100
+RESOLUTION = (256, 452)
+
+print(os.listdir(DATA_DIR))
---
+++ data/dataset.py
@@ -0,0 +1,30 @@
+import os
+import pandas as pd
+from torchvision.io import read_image
+
+import config
+
+class WAIC_TSC(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        # self.img_labels = pd.read_csv(annotations_file)
+        # self.img_dir = img_dir
+        # self.transform = transform
+        # self.target_transform = target_transform
+
+        self.img_dir = config.DATA_DIR
+        self.num_frames = config.NUM_FRAMES
+        self.transform = 
+
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
